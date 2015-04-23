eb0dafc6e4087a408c51488cada0791d4dc71005
Simon Gomizelj <sgomizelj@sangoma.com>
2015-04-23 16:03:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+## ansible-modules-nsc
+
+More of a proof-of-concept than anything else at the moment. Requires
+`delegated_to` to point to a machine where the [sangoma/safepy][1]
+library is installed. Hopefully in the future we'll ship it with our
+products simplifying this significantly.
+
+### Usage
+
+To demo this support, check out this repository and add the resulting directory
+to the [`ANSIBLE_LIBRARY`][2] environmental variable.
+
+Then a task like this should work inside your ansible script:
+
+~~~yaml
+---
+- name: Demo sip profile
+  nsc_profile:
+    host: "{{ inventory_hostname }}"
+    name: sample-sip-profile
+    ip: "{{ ansible_all_ipv4_addresses[0] }}"
+    port: 5060
+  delegate_to: 127.0.0.1
+~~~
+
+Hopefully a profile should be created. Still rough, still needs to
+support stuff like `state=started` and other profile fields.
+Eventually should expand to support stuff like trunks, dialplans, and
+other features.
+
+Feel free to request features/fixes.
+
+  [1]: https://github.com/sangoma/safepy
+  [2]: http://docs.ansible.com/developing_modules.html#module-paths
---
+++ nsc/nsc_profile.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import safepy
+import requests
+
+
+def get_network_ip(module, api):
+    obj = api.network.ip.list({'address': module.params['ip']})
+    if not obj:         
+        module.fail_json(msg="Provided ip does not exist on the device")
+    return obj[0]
+
+
+def get_api(module):
+    return safepy.api(module.params['host'])
+
+
+def dicts_differ(d1, d2):
+    overlap = d1.viewkeys() & d2.viewkeys()
+    for key in overlap:
+        if d1[key] != d2[key]:
+            return True
+    return False
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            host = dict(required=True),
+            name = dict(required=True),
+            ip = dict(required=True),
+            port = dict(required=True)
+        )
+    )
+
+    with get_api(module) as api:
+        profile_name = module.params['name']
+        profile_data = {'sip-ip': get_network_ip(module, api),
+                        'sip-port': module.params['port']}
+
+        current = api.sip.profile[profile_name]
+        try:
+            current_status = bool(current.status())
+            current_data = current.retrieve()
+        except requests.HTTPError:
+            # If we get an exception its because the profile doesn't
+            # exist on the product. We're free to create it.
+            api.sip.profile.create(profile_name, profile_data)
+        else:
+            # Otherwise we have to consider if its either already
+            # running or actually different to continue.
+            differ = dicts_differ(current_data, profile_data)
+            if not differ:
+                module.exit_json(changed=False)
+            elif current_status and differ:
+                module.fail_json(msg="Incompatable profile already started")
+
+            current.update(profile_data)
+
+    module.exit_json(changed=True)
+
+
+from ansible.module_utils.basic import *
+main()
