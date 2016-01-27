705acbdcceca732430547c6461997debc304ef25
Simon Gomizelj <sgomizelj@sangoma.com>
2016-01-27 15:55:22 -0500

    Fixup nsc_facts for localhost and better error handling

--- nsc_facts.py
+++ nsc_facts.py
@@ -3,23 +3,24 @@
 try:
     import safe
     import requests
-    HAS_SAFEPY=True
+    HAS_SAFEPY = True
 except ImportError:
-    HAS_SAFEPY=False
+    HAS_SAFEPY = False
 
 
 def main():
     module = AnsibleModule(
-        argument_spec=dict(
-            host=dict(required=True)
-        ),
-        supports_check_mode=False
+        argument_spec=dict()
     )
 
     if not HAS_SAFEPY:
         module.fail_json(msg="safepy2 is not installed")
 
-    api = safe.api(module.params['host'])
+    try:
+        api = safe.api('localhost')
+    except requests.HTTPError as e:
+        module.fail_json(msg=str(e))
+
     version = api.nsc.version.retrieve()
     version_facts = {'release': version['release_version'],
                      'major': version['major_version'],
@@ -29,7 +30,7 @@ def main():
 
     facts = {'nsc_product_name': version['full_name'],
              'nsc_version': version_facts}
-    module.exit_json(ansible_facts=facts)
+    module.exit_json(changes=False, ansible_facts=facts)
 
 
 from ansible.module_utils.basic import *
